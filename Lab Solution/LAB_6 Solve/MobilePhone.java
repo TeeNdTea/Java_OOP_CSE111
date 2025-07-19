ad7260a6802b785969f367f2d6a44366c01ed7f7
Tasnim Faizah <tee.faizah@gmail.com>
2025-07-20 00:04:58 +0600

    Add files via upload

---
+++ Lab Solution/LAB_6 Solve/MobilePhone.java
@@ -0,0 +1,51 @@
+public class MobilePhone {
+    public String[] names;
+    public int[] numbers;
+    public int contactCapacity;
+    public int totalContacts;
+
+    public MobilePhone() {
+        contactCapacity = 0;
+        totalContacts = 0;
+    }
+
+    public void setContactCapacity(int capacity) {
+        contactCapacity = capacity;
+        names = new String[contactCapacity];
+        numbers = new int[contactCapacity];
+    }
+
+    public void addContact(String name, int number) {
+        if (totalContacts < contactCapacity) {
+            names[totalContacts] = name;
+            numbers[totalContacts] = number;
+            System.out.println("The contact of " + name + " is added.");
+            totalContacts++;
+        } 
+        else {
+            System.out.println("Storage Full!!");
+        }
+    }
+
+    public void makeCall(int number) {
+        boolean found = false;
+        for (int i = 0; i < totalContacts; i++) {
+            if (numbers[i] == number) {
+                System.out.println("Calling " + names[i] + " . . .");
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Calling " + number + " . . .");
+        }
+    }
+
+    public void details() {
+        System.out.println("Total Contacts: " + totalContacts);
+        System.out.println("Contact List:");
+        for (int i = 0; i < totalContacts; i++) {
+            System.out.println(names[i] + ":" + numbers[i]);
+        }
+    }
+}
